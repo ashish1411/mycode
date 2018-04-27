37953b2ec71e9b5f297a006729e08b2579cd5490
ASHISH GAUR <ashishhartron123@gmail.com>
2018-04-27 13:59:44 +0530

    Initial application generated by JHipster-4.14.3

---
+++ src/main/webapp/app/entities/entity.state.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('examApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('entity', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
---
+++ src/main/webapp/app/services/auth/register.service.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('examApp')
+        .factory('Register', Register);
+
+    Register.$inject = ['$resource'];
+
+    function Register ($resource) {
+        return $resource('api/register', {}, {});
+    }
+})();
